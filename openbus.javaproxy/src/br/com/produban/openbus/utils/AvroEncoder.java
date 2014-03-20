0f7e5cbce61fee8b507390a70e900662803dd419
renato ochando <renato@10.3.0.121>
2014-03-20 11:59:08 -0300

    Writing the Kafka's Magic Byte and Schema ID for each message produced.

--- openbus.javaproxy/src/br/com/produban/openbus/utils/AvroEncoder.java
+++ openbus.javaproxy/src/br/com/produban/openbus/utils/AvroEncoder.java
@@ -2,9 +2,12 @@ package br.com.produban.openbus.utils;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.zip.CRC32;
 
 import kafka.producer.KeyedMessage;
 
+import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericDatumWriter;
 import org.apache.avro.io.Encoder;
 import org.apache.avro.io.EncoderFactory;
@@ -18,16 +21,31 @@ public class AvroEncoder {
 
 	public static KeyedMessage<String, byte[]> getKeyedMessage(AgentData agentData) {
 		AgentDataAvro agentDataAvro = parseAgentDataToAvro(agentData);
-
-		GenericDatumWriter<AgentDataAvro> writer = new GenericDatumWriter<AgentDataAvro>();
-		writer.setSchema(agentDataAvro.getSchema());
+		Schema schema = agentDataAvro.getSchema();
 		
+		GenericDatumWriter<AgentDataAvro> writer = new GenericDatumWriter<AgentDataAvro>();
+		writer.setSchema(schema);
+
 		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 
-		Encoder e = EncoderFactory.get().binaryEncoder(outputStream, null);
+		CRC32 crc32 = new CRC32();
+		crc32.reset();
+		crc32.update(schema.toString().getBytes());
+
+		ByteBuffer bufferSchemaId = ByteBuffer.allocate(8);
+		bufferSchemaId.putLong(crc32.getValue());
 
 		try {
+			// Writes magic byte
+			outputStream.write(0x0);
+			
+			// Writes Schema Id based on the CRC32 Schema
+			outputStream.write(bufferSchemaId.array());
+
+			Encoder e = EncoderFactory.get().binaryEncoder(outputStream, null);
+
 			writer.write(agentDataAvro, e);
+
 			e.flush();
 		} catch (IOException e1) {
 			e1.printStackTrace();
@@ -38,15 +56,14 @@ public class AvroEncoder {
 				outputStream.toByteArray());
 
 	}
-	
+
 	private static AgentDataAvro parseAgentDataToAvro(AgentData agentData) {
 		AgentDataAvro agentDataAvro = AgentDataAvro.newBuilder().setHost(agentData.getHost())
-		.setHostMetadata(agentData.getHost_metadata()).setKey(agentData.getKey())
-		.setEventid(agentData.getEventid()).setMtime(agentData.getMtime())
-		.setSeverity(agentData.getSeverity()).setSource(agentData.getSource())
-		.setState(agentData.getState()).setTimestamp(agentData.getTimestamp())
-		.setValue(agentData.getValue()).setClock(agentData.getClock())
-		.setLastlogsize(agentData.getLastlogsize()).setNs(agentData.getNs()).build();
+				.setHostMetadata(agentData.getHost_metadata()).setKey(agentData.getKey())
+				.setEventid(agentData.getEventid()).setMtime(agentData.getMtime()).setSeverity(agentData.getSeverity())
+				.setSource(agentData.getSource()).setState(agentData.getState()).setTimestamp(agentData.getTimestamp())
+				.setValue(agentData.getValue()).setClock(agentData.getClock())
+				.setLastlogsize(agentData.getLastlogsize()).setNs(agentData.getNs()).build();
 		return agentDataAvro;
 	}
 }
