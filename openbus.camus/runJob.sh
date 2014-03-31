6ed4a2595b408c84e6afe067cc9fa30cbe7d149d
makotoiguchi <daniel.iguchi@gmail.com>
2014-03-31 16:25:13 -0300

    shell script to run Camus' job

---
+++ openbus.camus/runJob.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# ~/camus/
+#		camus.properties
+#		log4j.xml
+#		openbus.camus-0.0.1-SNAPSHOT-shaded.jar
+#		runJob.sh
+#		camus/schemas:
+#			openbus-model-0.0.1-SNAPSHOT.jar
+#
+
+export HADOOP_CLASSPATH=$(pwd)/schemas/openbus-model-0.0.1-SNAPSHOT.jar
+echo HADOOP_CLASSPATH = $HADOOP_CLASSPATH
+
+hadoop jar openbus.camus-0.0.1-SNAPSHOT-shaded.jar com.linkedin.camus.etl.kafka.CamusJob -P camus.properties
+
